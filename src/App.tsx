8dbd6f05c1d97c122a409ce3120a73b3599a053a
alvinarkansas <alvinarkansas@gmail.com>
2025-04-19 11:20:12 +0700

    feat: add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AnimeList } from "./pages/AnimeList";
 import { AnimeDetail } from "./pages/AnimeDetail";
+import { NotFound } from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<AnimeList />} />
           <Route path="/anime/:id" element={<AnimeDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { Button } from "../components/Button";
+
+export const NotFound = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-6 min-h-screen">
+      <div>
+        <h1 className="text-center text-7xl font-bold text-ntrl-400 mb-2">
+          Sorry
+        </h1>
+        <p className="text-center">We can't seem to find this page</p>
+      </div>
+      <Link to="/">
+        <Button>Take me back, senpai</Button>
+      </Link>
+    </div>
+  );
+};
